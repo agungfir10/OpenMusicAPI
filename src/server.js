e97bf69d35ff2a378a7d96e3ba006e6d5a72dfb9
Agung Firmansyah <agungfirid@gmail.com>
2021-11-27 09:45:48 +0700

    refactor:(agungfir) add starter structure folder

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log('Open Music API');
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
