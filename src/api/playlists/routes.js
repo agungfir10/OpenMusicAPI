ddc2fe5a5f9e6e4cc63c7672ab053b4d31e09fb4
Agung Firmansyah <agungfirid@gmail.com>
2022-01-25 07:45:17 +0700

    playlist songs service

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -4,7 +4,7 @@ const routes = (handler) => [
         path: '/playlists',
         handler: handler.postPlaylistHandler,
         options: {
-            auth: 'notesapp_jwt',
+            auth: 'openmusicapp_jwt',
         },
     },
     {
@@ -12,41 +12,33 @@ const routes = (handler) => [
         path: '/playlists',
         handler: handler.getPlaylistsHandler,
         options: {
-            auth: 'notesapp_jwt',
+            auth: 'openmusicapp_jwt',
         },
     },
-    {
-        method: 'DELETE',
-        path: '/playlists/{id}',
-        handler: handler.deletePlaylistHandler,
-        options: {
-            auth: 'notesapp_jwt',
-        },
-    },
-    {
-        method: 'POST',
-        path: '/playlists/{id}/songs',
-        handler: handler.postSongPlaylist,
-        options: {
-            auth: 'notesapp_jwt',
-        },
-    },
-    {
-        method: 'GET',
-        path: '/playlists/{id}/songs',
-        handler: handler.getSongsPlaylist,
-        options: {
-            auth: 'notesapp_jwt',
-        },
-    },
-    {
-        method: 'DELETE',
-        path: '/playlists/{id}/songs',
-        handler: handler.deleteSongPlaylist,
-        options: {
-            auth: 'notesapp_jwt',
-        },
-    },
+    // {
+    //     method: 'DELETE',
+    //     path: '/playlists/{id}',
+    //     handler: handler.deletePlaylistHandler,
+    //     options: {
+    //         auth: 'openmusicapp_jwt',
+    //     },
+    // },
+    // {
+    //     method: 'GET',
+    //     path: '/playlists/{id}/songs',
+    //     handler: handler.getSongsPlaylist,
+    //     options: {
+    //         auth: 'openmusicapp_jwt',
+    //     },
+    // },
+    // {
+    //     method: 'DELETE',
+    //     path: '/playlists/{id}/songs',
+    //     handler: handler.deleteSongPlaylist,
+    //     options: {
+    //         auth: 'openmusicapp_jwt',
+    //     },
+    // },
 ];
 
 module.exports = routes;
---
+++ src/services/postgres/PlaylistSongsService.js
@@ -0,0 +1,132 @@
+/* eslint-disable quotes */
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
+const ClientError = require('../../exceptions/ClientError');
+
+class PlaylistSongsService {
+    constructor() {
+        this._pool = new Pool();
+    }
+
+    async addPlaylistSongs(playlistId, songId) {
+        const id = `playlist-songs-${nanoid(16)}`;
+        const query = {
+            text: 'INSERT INTO playlist_songs VALUES ($1, $2, $3) RETURNING song_id',
+            values: [id, playlistId, songId]
+        };
+
+        const { rows, rowCount } = await this._pool.query(query);
+
+        if (!rowCount) {
+            throw new InvariantError('Song gagal ditambahkan ke playlist');
+        }
+
+        return rows[0].song_id;
+    }
+
+    async deleteSongFromPlaylistById(playlistId, songId) {
+        const query = {
+            text: 'DELETE FROM playlist_songs WHERE playlist_id = $1 AND song_id = $2 RETURNING id',
+            values: [playlistId, songId]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (!rows.length) {
+            throw new NotFoundError('Lagu gagal di hapus dari playlist. lagu tidak ditemukan');
+        }
+    }
+
+    async deletePlaylistById(playlistId) {
+        const query = {
+            text: 'DELETE FROM playlists WHERE id = $1 RETURNING id',
+            values: [playlistId]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (!rows.length) {
+            throw new NotFoundError('Playlist gagal di hapus dari playlist. playlist tidak ditemukan');
+        }
+    }
+
+    async deletePlaylistSongsByPlaylistId(playlistId) {
+        const query = {
+            text: 'DELETE FROM playlist_songs WHERE playlist_id = $1 RETURNING id',
+            values: [playlistId]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (!rows.length) {
+            throw new NotFoundError('Playlist songs gagal di hapus dari playlist. playlist songs tidak ditemukan');
+        }
+    }
+
+    async getSongsPlaylist(playlistId) {
+        const query = {
+            text: 'SELECT songs.id, songs.title, songs.performer FROM playlist_songs LEFT JOIN songs ON playlist_songs.song_id = songs.id WHERE playlist_id = $1',
+            values: [playlistId]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (!rows.length) {
+            throw NotFoundError('Playlist tidak ditemukan');
+        }
+
+        return rows;
+    }
+
+    async checkSongExist(id) {
+        const query = {
+            text: 'SELECT * FROM songs WHERE id = $1',
+            values: [id]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (!rows.length) {
+            throw new NotFoundError('Lagu tidak ditemukan');
+        }
+    }
+
+    async verifyPlaylistOwner(id, owner) {
+        const query = {
+            text: 'SELECT playlists.id, playlists.name, playlists.owner, users.username FROM playlists LEFT JOIN users ON playlists.owner = users.id WHERE playlists.id = $1',
+            values: [id]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (!rows.length) {
+            throw new NotFoundError('Playlist tidak ditemukan');
+        }
+
+        const playlist = rows[0];
+
+        if (playlist.owner !== owner) {
+            throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+        }
+
+        return rows[0];
+    }
+
+    async checkSongAndPlaylistExist(playlistId, songId) {
+        const query = {
+            text: 'SELECT * FROM playlist_songs WHERE playlist_id = $1 AND song_id =  $2',
+            values: [playlistId, songId]
+        };
+
+        const { rows } = await this._pool.query(query);
+
+        if (rows.length > 0) {
+            throw new ClientError('Lagu sudah tersimpan di playlist');
+        }
+    }
+}
+
+module.exports = PlaylistSongsService;
