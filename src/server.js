e3b92aaf4608b07fa89f8495ce2be225c7d0715c
Agung Firmansyah <agungfirid@gmail.com>
2022-01-26 15:53:52 +0700

    delete many folder

--- src/server.js
+++ src/server.js
@@ -28,7 +28,6 @@ const init = async () => {
     },
   });
 
-
   await server.register([
     {
       plugin: songs,
--- src/validator/collaborations/schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const CollaborationsSchema = Joi.object({
-    playlistId: Joi.string().required(),
-    userId: Joi.string().required(),
-});
-
-module.exports = { CollaborationsSchema };
